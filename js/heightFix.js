e6ac734c8ebf2f58820720af77636d4696c1567b
blissman <blissman@gmail.com>
2017-04-29 17:31:40 -0400

    update profiles

--- js/heightFix.js
+++ js/heightFix.js
@@ -7,7 +7,6 @@ jQuery(
           You can set multiple things here.
         */
         $("#hero").css("height", $( window ).height() + "px")
-        $("#team").css("height", $( window ).height() + "px")
         $("#webapp").css("height", $( window ).height()*0.8 + "px")
         $("#webapp").css("padding", $( window ).height()*0.05 + "px")
         $("#map").css("height", $( window ).height()*0.7 + "px")
