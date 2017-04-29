639e0b9a79046a4b103f27db12143d61b2a9b9de
blissman <blissman@gmail.com>
2017-04-29 14:37:16 -0400

    add team section and hero section to heightFix

--- js/heightFix.js
+++ js/heightFix.js
@@ -6,6 +6,8 @@ jQuery(
           This function detects and sets your section heights.
           You can set multiple things here.
         */
+        $("#hero").css("height", $( window ).height() + "px")
+        $("#team").css("height", $( window ).height() + "px")
         $("#webapp").css("height", $( window ).height()*0.8 + "px")
         $("#webapp").css("padding", $( window ).height()*0.05 + "px")
         $("#map").css("height", $( window ).height()*0.7 + "px")
